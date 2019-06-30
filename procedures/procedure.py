e1f276b6acf51b6827e36114bcd52e76d1ab6cff
Christy42 <mark.christiansen@nuim.ie>
2019-06-30 18:02:07 +0100

    Create basic procedures for forward propogation

--- procedures/procedure.py
+++ procedures/procedure.py
@@ -1,9 +1,9 @@
 
 class Procedure:
 
-    def __init__(self, game):
-        self.game = game
-        self.game.state.stack.push(self)
+    def __init__(self, neural_net):
+        self.neural_net = neural_net
+        self.neural_net.state.stack.push(self)
         self.done = False
         self.initialized = False
 
@@ -13,5 +13,25 @@ class Procedure:
     def step(self, action):
         pass
 
-    def available_actions(self):
+
+class ForwardProp(Procedure):
+    def __init__(self, neural_net):
+        super().__init__(neural_net)
+
+    def step(self, action):
+        for i in range(1, self.neural_net):
+            pass
+
+
+class Sigmoid(Procedure):
+    def __init__(self, neural_net):
+        super().__init__(neural_net)
+
+    def step(self, action):
         pass
+
+
+class BackwardProp(Procedure):
+    def __init__(self, neural_net):
+        super().__init__(neural_net)
+
